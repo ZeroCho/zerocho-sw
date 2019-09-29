c36af1521c8c6718f59aba46442804348aa3badd
zerocho <zerohch0@gmail.com>
2019-09-29 21:12:25 +0900

    Update: resizer made

--- img-to-ascii/index.js
+++ img-to-ascii/index.js
@@ -3,15 +3,14 @@ const quantize = require('quantize');
 const pixels = require('image-pixels');
 const output = require('image-output');
 const fs = require('fs');
-const pngjs = require('pngjs');
 
-const { PNG } = pngjs;
-
-const horizontalDivisor = 10;
-const verticalDivisor = 10;
+const horizontalDivisor = 40;
+const verticalDivisor = 40;
 const filePath = './zerocho.jpg';
+const color = true;
+
 const ascii = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
-console.log(ascii);
+
 const main = async () => {
   const { data, width, height } = await pixels(filePath);
   const widthSection = Math.ceil(width / horizontalDivisor);
@@ -36,28 +35,37 @@ const main = async () => {
       // i < 2 && j < 2 && console.log(i, j, arrayOfPixels);
       // console.log(i, j, palette);
       matrix[i][j] = [palette[0][0], palette[0][1], palette[0][2], 255];
+      if (!color) {
+        const rgb = matrix[i][j];
+        let b = 0.299 * rgb[0] + 0.587 * rgb[1] + 0.11 * rgb[2];
+        b = b < 128 ? Math.max(0, b * 0.7) : Math.min(255, b * 1.4);
+        matrix[i][j] = [b, b, b];
+      }
     }
   }
   console.log(width, height, horizontalDivisor, verticalDivisor, flat(matrix, 2).length);
   // console.log('matrix', matrix);
   const brightness = matrix.map((row) => {
     return row.map((rgb) => {
-      const b = 0.2125862307855955516 * rgb[0] + 0.7151703037034108499 * rgb[1] + 0.07220049864333622685 * rgb[2];
-      return `<span style="color:rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]});">${ascii[Math.floor(b / ascii.length)]}</span>`;
+      if (color) {
+        const b = 0.2125862307855955516 * rgb[0] + 0.7151703037034108499 * rgb[1] + 0.07220049864333622685 * rgb[2];
+        return `<span style="color:rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]});">${ascii[Math.floor(b / ascii.length)]}</span>`;
+      }
+      return `<span>${ascii[Math.floor(rgb[0] / (255 / (ascii.length - 1)))]}</span>`;
     });
   });
   // console.log('flat', flat(matrix, 2).slice(0, 30));
-  console.log('brightness');
-  console.log(brightness.map((row) => row.join('')).join('<br/>'));
   fs.writeFileSync(
     'index.html',
-    `<pre style="transform: scale(0.25);line-height:6px">${brightness.map((row) => row.join('')).join('<br/>')}</pre>`
+    `<pre style="transform: scale(0.5);line-height:10px;letter-spacing: 3px">${brightness.map((row) => row.join('')).join('<br/>')}</pre>`
   );
   output({
     data: flat(matrix, 2),
     width: widthSection,
     height: heightSection,
   }, 'zerochoresult.jpg');
+  console.timeEnd('timer');
 };
 
+console.time('timer');
 main();
